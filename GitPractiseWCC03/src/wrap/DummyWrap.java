d0198a4093b42c412ebbcb7a75114fb6c05c40c9
Sumanth <Sumanth@TYSS-Sumanth>
2023-11-15 16:31:39 +0530

    Updated DummyWrap by adding 2 lines

--- GitPractiseWCC03/src/wrap/DummyWrap.java
+++ GitPractiseWCC03/src/wrap/DummyWrap.java
@@ -4,5 +4,7 @@ public class DummyWrap {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to GitHub");
+		System.out.println("Learning about history");
+		System.out.println("rolling back");
 	}
 }
